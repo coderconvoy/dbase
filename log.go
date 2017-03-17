dff5bad3a5b9cfad154a84e326b6314c1796cd2b
coderconvoy <thebeaky@psmail.net>
2017-03-17 11:47:33 +0000

    Adding logging stuff, WIP

--- log.go
+++ log.go
@@ -6,22 +6,63 @@ import (
 	"path"
 	"sync"
 	"time"
+
+	"github.com/pkg/errors"
 )
 
-type Logger struct {
+type Logger interface {
+	Log(string)
+}
+
+type FmtLog struct{}
+
+func (fl FmtLog) Log(m string) {
+	fmt.Println(m)
+}
+
+type FolderLog struct {
 	Folder string
 	sync.Mutex
 }
 
-var dLog = Logger{
-	Folder: "logs",
+var dlog Logger
+
+// QLog provides a quick log method, that requieres no setup to allow simple logging
+// If you want more fancy stuff, Use a LogGroup
+func QLog(m string) error {
+	if dlog == nil {
+		dlog, err := NewLogGroup("logs")
+		if err != nil {
+			return errors.Wrap(err, "Could not log")
+		}
+	}
+	dlog.Log(m)
+	return nil
 }
 
-func DLog(m string) {
-	dLog.Log(m)
+// SetQLogFolder changes the default folder for QLog default "logs"
+// returns an error if it cannot prepare the directory
+func SetQLogFolder(f string) error {
+	nlog, err := NewLogGroup(f)
+	if err != nil {
+		return errors.Wrap(err, "Could not set Log Folder")
+	}
+	dlog = nlog
+	return nil
 }
 
-func (l *Logger) Log(m string) {
+func NewFolderLog(f string) (*FolderLog, error) {
+
+	err := os.MkdirAll(f, 0664)
+	if err != nil {
+		return nil, errors.Wrap(err, "Could not create Logger")
+	}
+
+	return &FolderLog{Folder: f}, nil
+
+}
+
+func (l *FolderLog) Log(m string) {
 	l.Lock()
 	defer l.Unlock()
 	now := time.Now()
@@ -40,11 +81,61 @@ func (l *Logger) Log(m string) {
 	if err != nil {
 		fmt.Println("message not logged: ", err, "::", m)
 	}
-
 }
 
 type LogGroup struct {
-	Main Logger
+	main Logger
 	logs map[string]Logger
 	sync.Mutex
+}
+
+func NewLogGroup(f string) (*LogGroup, error) {
+	var main Logger
+	main, err := NewFolderLog(f)
+
+	if err != nil {
+		main = FmtLog{}
+	}
+
+	return &LogGroup{
+		main: main,
+		logs: make(map[string]Logger),
+	}, nil
+}
+func (lg *LogGroup) SetMain(l Logger) {
+	lg.Lock()
+	lg.main = l
+	lg.Unlock()
+}
+
+func (lg *LogGroup) AddLogger(k string, l Logger) {
+	lg.Lock()
+	lg.logs[k] = l
+	lg.Unlock()
+
+}
+
+func (lg *LogGroup) AddFolderLog(k, fol string) error {
+	lg.Lock()
+	fl, err := NewFolderLog(fol)
+	if err != nil {
+		return errors.Wrap(err)
+	}
+	lg.logs[k] = &Logger{Folder: v}
+	lg.Unlock()
+}
+
+func (lg *LogGroup) Log(m) {
+	lg.main.Log(m)
+}
+
+func (lg *LogGroup) LogTo(k, m string) {
+	lg.Lock()
+	defer lg.Unlock()
+
+	lg.main.Log(k + m)
+	mini, ok := lg.logs[k]
+	if ok {
+		mini.Log(m)
+	}
 }
--- logs_test.go
+++ logs_test.go
@@ -5,3 +5,7 @@ import "testing"
 func Test_log(t *testing.T) {
 	DLog("Hello world")
 }
+
+func Test_LGroup(t *testing.T) {
+	lg := New
+}
